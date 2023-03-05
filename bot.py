72b563af94df5089c961e79bdf3ee9929c00e7c6
Timofey Zabelin <83931815+tzabelin@users.noreply.github.com>
2023-03-06 00:51:29 +0200

    Update bot.py
    Translated

--- bot.py
+++ bot.py
@@ -19,19 +19,11 @@ print("nick:", botNick)
 # In all other places characters
 # _ * [ ] ( ) ~ ` > # + - = | { } . ! 
 # must be escaped with the preceding character '\'.
-def start(update, context): # 当用户输入/start时，返回文本
+def start(update, context): # Return text when user enters /start
     user = update.effective_user
     update.message.reply_html(
         rf"Hi {user.mention_html()} ! I am an Assistant, a large language model trained by OpenAI. I will do my best to help answer your questions.",
     )
-
-def help(update, context):
-    message = (
-        "我是人见人爱的 ChatGPT\~\n\n"
-        "欢迎访问 https://github\.com/yym68686/ChatGPT\-Telegram\-Bot 查看源码\n\n"
-        "有 bug 可以联系 @yym68686"
-    )
-    update.message.reply_text(message, parse_mode='MarkdownV2')
 
 def reset(update, context):
     chatbot.reset_chat()
@@ -66,14 +58,14 @@ def process_message(update, context):
                     chatbot.reset_chat()
                     message = context.bot.send_message(
                         chat_id=chat_id,
-                        text="对话已超过上限，已重置聊天，请重试！",
+                        text="Conversation has exceeded the limit, chat has been reset, please try again！",
                     )
                     return
                 if "conversation_id" in data:
                     continue
                 message = context.bot.send_message(
                     chat_id=chat_id,
-                    text="未知错误：" + str(data),
+                    text="Unknown error：" + str(data),
                 )
                 return
         context.bot.send_message(
@@ -88,7 +80,7 @@ def process_message(update, context):
         print("Exception str", str(e))
         context.bot.send_message(
             chat_id=chat_id,
-            text="出错啦 :(",
+            text="Error! :(",
             parse_mode=ParseMode.MARKDOWN,
         )
 
@@ -96,22 +88,22 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     if ("can't" in str(context.error)):
         message = (
-            f"出错啦！请重试。\n\n"
+            f"There's an error! Please try again.\n\n"
         )
         context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode='MarkdownV2')
 
-def unknown(update: Update, context: CallbackContext): # 当用户输入未知命令时，返回文本
+def unknown(update: Update, context: CallbackContext): # Return text when the user enters an unknown command
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 def setup(token):
-    if MODE == "dev": # 本地调试，需要挂代理，这里使用的是 surge
+    if MODE == "dev": # Local debugging, need to hang proxy, here use surge
         updater = Updater(token, use_context=True, request_kwargs={
-            'proxy_url': 'http://127.0.0.1:6152' # 需要代理才能使用 telegram
+            'proxy_url': 'http://127.0.0.1:6152' # Proxy required to use telegram
         })
-    elif MODE == "prod": # 生产服务器在美国，不需要代理
+    elif MODE == "prod": # Production servers in the US, no proxy required
         updater = Updater(token, use_context=True)
     else:
-        logger.error("需要设置 MODE!")
+        logger.error("Need to set MODE!")
         sys.exit(1)
 
     # set commands
@@ -124,7 +116,6 @@ def setup(token):
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("reset", reset))
-    dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(MessageHandler(Filters.text, process_message))
     dispatcher.add_handler(MessageHandler(Filters.command, unknown))
     dispatcher.add_error_handler(error)
